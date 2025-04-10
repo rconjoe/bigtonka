c519311c9894f1dca288ccaf22718702daec7156
rconjoe <root@trog.codes>
2025-04-09 23:53:31 -0700

    move to getAll instead of just linktree data

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,10 +1,9 @@
-export default async function linktree() {
+export default async function getAll() {
   try {
     const results = await fetch(
-      "https://amiable-perfection-production.up.railway.app/api/linktree",
+      "https://amiable-perfection-production.up.railway.app/api/getAll",
     );
     const response = await results.json();
-    console.log(response);
     return response;
   } catch (error) {
     console.log(error);
