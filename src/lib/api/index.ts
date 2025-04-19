17c4af9b5eabc016d48273beee41cdffd9159fdc
rconjoe <root@trog.codes>
2025-04-18 21:57:54 -0700

    add comment

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,6 +1,7 @@
 export default async function getAll() {
   try {
     const results = await fetch(
+      // env var? this is to a railway fcn
       "https://amiable-perfection-production.up.railway.app/api/getAll",
     );
     const response = await results.json();
