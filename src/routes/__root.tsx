6db49dad405846331dca4a9e8722e638372638ea
rconjoe <root@trog.codes>
2025-06-20 19:26:49 -0700

    reenable tanstack dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,12 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-// import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
-// import Header from '../components/Header'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      {/* <Header /> */}
 
       <Outlet />
-      {/* <TanStackRouterDevtools /> */}
+      <TanStackRouterDevtools />
     </>
   ),
 })
