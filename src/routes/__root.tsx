e85a6fb587f113c818084fb9578c7bc1eb5d5b7d
rconjoe <root@trog.codes>
2025-08-03 17:39:46 -0700

    add basic metadata and setup favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,27 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: 'description',
+        content: 'The official Big Tonka website',
+      },
+      {
+        title: 'BIG TONK',
+      },
+    ],
+    links: [
+      {
+        rel: 'icon',
+        href: '../../public/favicon.ico'
+      }
+    ],
+  }),
   component: () => (
     <>
-
+      <HeadContent />
       <Outlet />
       <TanStackRouterDevtools />
     </>
