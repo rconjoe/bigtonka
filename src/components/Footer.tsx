e23833ae47e8b6fe07d2b6f7fe735cdeb3c7db8f
rconjoe <root@trog.codes>
2025-08-14 15:31:09 -0700

    update ig link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,17 +4,8 @@ const Footer = () => {
       className="w-full max-w-2xl lg:max-w-4xl mx-auto px-4 mt-16 pb-8
       flex flex-col items-center text-center"
     >
-      {/* <a */}
-      {/*   href="https://www.buymeacoffee.com/yourprofile" // <--- REPLACE WITH YOUR BUYMEACOFFEE LINK */}
-      {/*   target="_blank" */}
-      {/*   rel="noopener noreferrer" */}
-      {/*   className="text-white font-tektur font-semibold text-lg md:text-xl mb-2 */}
-      {/*     hover:text-white/80 transition-colors duration-300" */}
-      {/* > */}
-      {/*   Click Here to Support The Rideouts */}
-      {/* </a> */}
       <a
-        href="https://www.instagram.com/trogdooor" // <--- REPLACE WITH YOUR INSTAGRAM LINK
+        href="https://www.instagram.com/joebullettt"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white/60 font-tektur text-sm md:text-base
