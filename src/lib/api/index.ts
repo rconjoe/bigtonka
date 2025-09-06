f6639eb0203414458450262f9ba60813a7470ac8
rconjoe <root@trog.codes>
2025-09-06 12:32:00 -0700

    this is shit and temporary but adds products to the getAll api caller

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,10 +1,15 @@
+import getProducts from "./products";
+
 export default async function getAll() {
-  const api_url = import.meta.env.VITE_API_URL
+  const api_url = import.meta.env.VITE_API_URL;
   try {
-    const results = await fetch(
-      api_url
-    );
-    const response = await results.json();
+    const _content = await fetch(api_url);
+    const content = await _content.json();
+    const products = await getProducts();
+    const response = {
+      content,
+      products,
+    };
     return response;
   } catch (error) {
     console.log(error);
